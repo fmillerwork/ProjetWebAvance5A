a566b36a9db9bcfda417eae42b88c4bdb317ca7c
TFache <thom.fache@orange.fr>
2021-10-05 10:41:17 +0200

    Made some tests

--- ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
+++ ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.ProfileService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ProfileServiceApplicationTests {
+
+	@Autowired
+	public MockMvc mockMvc;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void getProfilesShouldReturnEmptyArray() throws Exception {
+		this.mockMvc.perform(get("/PS/profiles",1))
+		.andDo(print())
+		.andExpect(status().isOk())
+		.andExpect(content().json("[]"));
+	}
+
+	@Test
+	void putProfileShoudSucceed() throws Exception {
+		Profile profile = new Profile(1,"APERO","apero@gmail.com","");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String profile_json = objectMapper.writeValueAsString(profile);
+
+		this.mockMvc.perform(put("/PS/profiles")
+					.content(profile_json)
+					.contentType(MediaType.APPLICATION_JSON)
+					)
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
 }
