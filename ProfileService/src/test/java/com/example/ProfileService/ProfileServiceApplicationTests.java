fc97d9c1b4b42d07508b89080d5c34243df4c3c1
fmillerwork <florian.miller.work@gmail.com>
2021-10-05 10:38:24 +0200

    Ajout TU

--- ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
+++ ProfileService/src/test/java/com/example/ProfileService/ProfileServiceApplicationTests.java
@@ -1,13 +1,47 @@
 package com.example.ProfileService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class ProfileServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	public void getProfiles_shouldReturnEmptyArray() throws Exception{
+		this.mockMvc.perform(get("/PS/profiles"))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().json("[]"));
+	}
+
+	@Test
+	public void putProfiles_shouldSucceed() throws Exception{
+		Profile profile = new Profile(1,"Florian","florian.miller@gmail.com","");
+		ObjectMapper objectMapper = new ObjectMapper();
+		String profile_json = objectMapper.writeValueAsString(profile);
+
+		this.mockMvc.perform(post("/PS/profiles")
+						.content(profile_json)
+						.contentType(MediaType.APPLICATION_JSON)
+						)
+					.andDo(print())
+				.andExpect(status().isOk());
 	}
 
 }
