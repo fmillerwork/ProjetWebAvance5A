c83cfe7aecc3cd277c6260a4d48b954b638c74e0
fmillerwork <florian.miller.work@gmail.com>
2021-10-05 09:20:42 +0200

    Ajout messages pour HttpMessageNotReadableException

--- ProfileService/src/main/java/com/example/ProfileService/Exception/ProfileExceptionController.java
+++ ProfileService/src/main/java/com/example/ProfileService/Exception/ProfileExceptionController.java
@@ -1,6 +1,8 @@
 package com.example.ProfileService.Exception;
 
+import org.springframework.boot.json.JsonParseException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -12,14 +14,14 @@ import java.util.Map;
 @RestControllerAdvice
 public class ProfileExceptionController {
 
-    @ResponseBody
+    @ResponseBody // not necesarry with @RestControllerAdvice
     @ExceptionHandler(ProfileNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     String profileNotFoundHandler(ProfileNotFoundException ex){
         return ex.getMessage();
     }
 
-    @ResponseBody
+    @ResponseBody // not necesarry with @RestControllerAdvice
     @ExceptionHandler(EmailInUseException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
     String emailInUseHandler(EmailInUseException ex){
@@ -28,7 +30,7 @@ public class ProfileExceptionController {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+    public Map<String, String> validationExceptionHandler(MethodArgumentNotValidException ex){
         Map<String, String> errors = new HashMap<>();
         for (ObjectError error : ex.getBindingResult().getAllErrors()){
             String fieldName = ((FieldError)error).getField();
@@ -36,5 +38,11 @@ public class ProfileExceptionController {
             errors.put(fieldName, errorMessage);
         }
         return errors;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String notReadableHttpMessageHandler(HttpMessageNotReadableException ex){
+        return ex.getMessage();
     }
 }
