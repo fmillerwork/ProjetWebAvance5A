9c72748f92dcd3759b6b8a31ac5879683178ac63
TFache <thom.fache@orange.fr>
2021-12-07 09:02:38 +0100

    Docker Compose set up. Needed to hardcode the profile service in Gateway\...\application.properties for this for some reason

--- Gateway/src/main/java/com/example/Gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/example/Gateway/GatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.Gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -9,6 +10,11 @@ import org.springframework.http.HttpMethod;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    @Value("${service.authentication}")
+    private String auth_service_url;
+    @Value("${service.profiles}")
+    private String profile_service_url;
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
@@ -20,23 +26,23 @@ public class GatewayApplication {
                 .route(p -> p
                         .path("/profiles").and().method(HttpMethod.GET)
                         .filters(f -> f.prefixPath("/PS"))
-                        .uri("http://localhost:8081"))
+                        .uri(profile_service_url))
                 .route(p -> p
                         .path("/profiles/{id}/token")
                         .filters(f -> f.rewritePath("/profiles", "/AS/users"))
-                        .uri("http://localhost:8080"))
+                        .uri(auth_service_url))
                 .route(p -> p
                         .path("/profiles/{id}/password")
                         .filters(f -> f.rewritePath("/profiles", "/AS/users"))
-                        .uri("http://localhost:8080"))
+                        .uri(auth_service_url))
                 .route(p -> p
                         .path("/profiles/{id}/{endpoint}")
                         .filters(f -> f.prefixPath("/PS"))
-                        .uri("http://localhost:8081"))
+                        .uri(profile_service_url))
                 .route(p -> p
                         .path("/login")
                         .filters(f -> f.prefixPath("/PS"))
-                        .uri("http://localhost:8081"))
+                        .uri(profile_service_url))
 
                 .build();
     }
